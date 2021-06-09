1409c45c792c80b7f780f8229cd7221ca40a37c5
1w50 111606 Ezequiel Visintini <1w50111606EzequielVisintini@users.noreply.github.com>
2021-06-09 10:39:34 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+
+//crear app de express
+const app = express(); //express es la variable de arriba, pero ahora como funcion. Contiene todo lo necesario  de express para crear el servidor
+
+//Ruta para el home
+app.listen(3000);
+
+app.use('/', (req, res) =>{
+    res.send('Hola')
+});
