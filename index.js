e737347c01a8620e4c8e7e20443fc7b2cf6d7516
1w50 111606 Ezequiel Visintini <1w50111606EzequielVisintini@users.noreply.github.com>
2021-06-09 10:42:57 -0300

    inicio

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require ('express');
 const app = express(); //express es la variable de arriba, pero ahora como funcion. Contiene todo lo necesario  de express para crear el servidor
 
 //Ruta para el home
-app.listen(3000);
-
 app.use('/', (req, res) =>{
     res.send('Hola')
 });
+
+app.listen(3000);
+
